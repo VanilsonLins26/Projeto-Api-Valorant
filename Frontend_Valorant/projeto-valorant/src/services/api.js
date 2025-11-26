088822907ac0e1284c81908c58a5c536318acb62
vanilson <vanilson.liins@gmail.com>
2025-11-25 23:44:36 -0300

    integrando o pagamento 4

--- Frontend_Valorant/projeto-valorant/src/services/api.js
+++ Frontend_Valorant/projeto-valorant/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8082';
+
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: baseURL
 })
 
 export default api
