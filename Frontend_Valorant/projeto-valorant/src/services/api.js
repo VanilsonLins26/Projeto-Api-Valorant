e6f3ae5d011109d8b2d00ed803d9095e232b0a26
vanilson <vanilson.liins@gmail.com>
2025-12-02 01:55:38 -0300

    atualizando caminho back end

--- Frontend_Valorant/projeto-valorant/src/services/api.js
+++ Frontend_Valorant/projeto-valorant/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://vibrant-nature-production.up.railway.app'
+    baseURL: 'gateway-production-d435.up.railway.app'
 })
 
 export default api
